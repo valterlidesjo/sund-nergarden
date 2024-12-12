0abe985a5bd6492205671e22ab9b6661d9212ef1
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2024-12-12 16:01:54 +0100

    added use imports to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import BackArrow from './components/ui/BackArrow/BackArrow'
-import BigBtn from './components/ui/BigBtn/BigBtn'
-import NavHamburger from './components/ui/NavHamburger/NavHamburger'
+import HorizontalScroll from "./components/HorizontalScroll";
+import BackArrow from "./components/ui/BackArrow/BackArrow";
+import BigBtn from "./components/ui/BigBtn/BigBtn";
+import NavHamburger from "./components/ui/NavHamburger/NavHamburger";
 
 function App() {
-
   return (
     <>
-      <NavHamburger/>
-      <BackArrow size='3.5rem'/>
-      <BigBtn text="Boka nu"/>
+      <NavHamburger />
+      <BackArrow size="3.5rem" />
+      <BigBtn text="Boka nu" />
+      <HorizontalScroll />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/HorizontalScroll.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HorizontalScroll = () => {
+  return (
+    <div className="scroll-container">
+      <p>balle</p>
+    </div>
+  );
+};
+
+export default HorizontalScroll;
