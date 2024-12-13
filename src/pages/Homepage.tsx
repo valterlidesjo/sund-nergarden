f86d0fdd94bb9ec64dc8946be503a7f1fe320a68
valterlidesjo <valle.lidesjo@gmail.com>
2024-12-13 09:17:28 +0100

    homepage styling

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,24 +1,68 @@
 import React from "react";
 import HorizontalScroll from "../components/HorizontalScroll";
-import sundCharkImage from "../assets/sund-chark.jpeg";
+import sundStartImage from "../assets/sund-hus1.jpeg";
+import sundVinImage from "../assets/sund-vin.jpeg";
+import "./scss/_homepage.scss";
+import SmaBtn from "../components/ui/SmaBtn/SmaBtn";
+import { images } from "./extra/HomepageImages";
 
-const images = [
-  { id: "1", src: sundCharkImage, alt: "Image 1", text: "test" },
-  { id: "1", src: sundCharkImage, alt: "Image 1", text: "test" },
-  { id: "1", src: sundCharkImage, alt: "Image 1", text: "test" },
-  { id: "1", src: sundCharkImage, alt: "Image 1", text: "test" },
-  { id: "1", src: sundCharkImage, alt: "Image 1", text: "test" },
-  { id: "1", src: sundCharkImage, alt: "Image 1", text: "test" },
-  { id: "1", src: sundCharkImage, alt: "Image 1", text: "test" },
-  { id: "1", src: sundCharkImage, alt: "Image 1", text: "test" },
-];
 
 const HomePage = () => {
   const handleImageClick = (id: string) => {
     alert(`Image clicked: ${id}`);
   };
 
-  return <HorizontalScroll images={images} onImageClick={handleImageClick} />;
+  return (
+    <>
+      <div className="homepage-pic-container">
+        <img src={sundStartImage} alt="Sund Start Image" />
+        <div className="homepage-text-container">
+          <p>Sund Nergården</p>
+        </div>
+      </div>
+      <div className="homepage-container">
+        <div className="homepage-text-container-1">
+          <p className="homepage-text-container-1">
+            Högt beläget vid sjön Sillen i vackra Sörmland hittar du Nergården.
+            Våra elva rum fördelat på sju dubbelrum och fyra juniorsviter har
+            varierande utsikt och planlösning men är inredda med samma
+            faciliteter och med en gemensam känsla. Under sommarmånaderna
+            erbjuder vi också tre populära Glamping.
+          </p>
+        </div>
+        <div className="homepage-wine-container">
+          <img src={sundVinImage} alt="Vinflaskor" />
+        </div>
+        <div className="homepage-age-limit-container">
+          <p>Hotellet och restaurangen har en åldersgräns på 18år.</p>
+        </div>
+        <div className="homepage-text-container-2">
+          <p className="homepage-text-container-2">
+            Tre rätters meny och annat trevligt ingår alltid, Barnfritt hotell,
+            Bastu och bad beroende på säsong, Laddning finns för elbil, Hunden
+            får följa med.
+          </p>
+        </div>
+        <div className="homepage-scroll-header">
+          <h1>Rum, Mat & Mera</h1>
+          <p>Allt detta ingår när du bokar rum eller glamping med oss!</p>
+        </div>
+      </div>
+      <HorizontalScroll images={images} onImageClick={handleImageClick}/>
+      <div className="homepage-container">
+      <div className="homepage-book-container">
+        <p>För lediga rum samt aktuella priser se vår bokningskalender</p>
+        <SmaBtn text="Bokningskalender"/>
+      </div>
+      <div className="homepage-text-container-3">
+        <p className="homepage-text-container-3">
+        Vi älskar mat & dryck som serveras i en vacker och trevlig miljö, så varmt välkommen till oss!
+        </p>
+        
+      </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
---
+++ src/pages/extra/HomepageImages.ts
@@ -0,0 +1,13 @@
+import sundCharkImage from "../../assets/sund-chark.jpeg";
+import sundPoolImage from "../../assets/sund-pool.jpeg";
+import sundMatImage from "../../assets/sund-mat.jpeg";
+import sundFrukostImage from "../../assets/sund-frukost.jpg";
+import sundDrinkImage from "../../assets/sund-drink.jpg";
+
+export const images = [
+    { id: "1", src: sundCharkImage, alt: "Image 1", text: "CHARK" },
+    { id: "1", src: sundPoolImage, alt: "Image 2", text: "UTOMHUS POOL" },
+    { id: "1", src: sundDrinkImage, alt: "Image 3", text: "FÖRDRINK" },
+    { id: "1", src: sundMatImage, alt: "Image 4", text: "MAT" },
+    { id: "1", src: sundFrukostImage, alt: "Image 5", text: "FRUKOST" },
+  ];
