68e234782424c4cb304e208943142e8ae0aecc0c
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2024-12-13 14:25:55 +0100

    functionallity for slideshow added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import NavMobile from "./components/NavMobile";
+import RoomPageComponent from "./components/RoomPageComponent";
 import NavHamburger from "./components/ui/NavHamburger/NavHamburger";
 import SmaBtn from "./components/ui/SmaBtn/SmaBtn";
 import HomePage from "./pages/Homepage";
+import Test from "./pages/Test";
 
 function App() {
   return (
     <>
       <NavMobile />
-      <HomePage />
+      {/* <HomePage /> */}
+      <RoomPageComponent />
     </>
   );
 }
