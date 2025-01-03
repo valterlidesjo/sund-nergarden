62d45cf4479c187ac9b4d0de49b8b9688b6dd258
valterlidesjo <valle.lidesjo@gmail.com>
2025-01-03 09:27:31 +0100

    search but no styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App';
+import "react-datepicker/dist/react-datepicker.css";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/pages/Rummen.tsx
+++ src/pages/Rummen.tsx
@@ -7,6 +7,7 @@ import { glampingImages } from "./extra/GlampingImages";
 import TextSection from "../components/TextSection";
 import { useNavigate } from "react-router-dom";
 import sundRitad from "../assets/sund-ritad.jpeg";
+import SearchBooking from "../components/SearchBooking";
 
 const Rummen = () => {
     const goToBooking = () => {
@@ -43,13 +44,6 @@ const Rummen = () => {
       </div>
       <HorizontalScroll images={roomImages} onImageClick={(image) => goToRoom(image.alt)} backgroundCol="#D3E2D5"/>
       <div className="rummen-container">
-        {/* <div className="homepage-book-container">
-          <p>För lediga rum samt aktuella priser se vår </p>
-          <div className="homepage-book-text-container">
-            <p>bokningskalender</p>
-            <SmaBtn text="Bokningskalender" click={goToBooking}/>
-          </div>
-        </div> */}
         <TextSection text="Våra tre Glamping tält" fontSize="2rem" margin="3rem 0 2rem 0"/>
         <TextSection text="Möblerade med riktiga sängar som är bäddade med fluffiga täcken och kuddar med sänglinne av ekologisk bomullssatin. 
         Inredningen är så att du får känslan av att du bor i ett hotellrum fast du är mitt i naturen. På den tillhörande terrassen finner du en sittgrupp samt en varm & kall utomhusdusch med produkter från Björk and Berries. Toaletter hittar du ca 50m bort från Glampingen. Tälten är av 100% bomullscanvas som gör att tältens duk andas naturligt och är vädertåliga mot regn och blåst och håller fukten borta. Det finns ett elektriskt oljeelement för eventuellt kyliga nätter. 
@@ -57,6 +51,7 @@ const Rummen = () => {
         <TextSection text="Tälten" fontSize="2.5rem" margin="0" />
       </div>
       <HorizontalScroll images={glampingImages} onImageClick={(image) => goToRoom(image.alt)}/>
+      <SearchBooking />
       <div className="rummen-container">
         <TextSection text="Max 2 personer - Glampingsäsong: 1/6 -24 t.o.m 31/8 -24." fontSize="1rem" padding="0 1rem" textAlign="center"/>
         <TextSection text="Tips! Eventuella avbokningar läggs även ut på vår Instagram Story @Sundnergarden, så följ oss gärna där."  padding="0 2rem" textAlign="center"/>
